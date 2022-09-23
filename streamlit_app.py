487e1ef9ce8ba786dc68a15775a6c98731c13245
Magdalena <114231586+magdalenas0@users.noreply.github.com>
2022-09-23 13:58:03 +0200

    Update streamlit_app.py
    Added emots

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
-streamlit.header("Breakfast menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.header("🥣 Breakfast menu")
+streamlit.text("🥗 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🐔 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🥑🍞 Hard-Boiled Free-Range Egg")
